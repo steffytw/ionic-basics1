cbcbf47dc8ef86ed10a5da530f7bbd61f8ed19e6
steffytw <steffythankamwilson@cs.ajce.in>
2019-11-19 11:27:59 +0530

    sixth  commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const reasonInput =document.querySelector("#input-reason");
+const amountInput =document.querySelector("#input-amount");
+const cancelBtn =document.querySelector("#btn-cancel");
+const confirmBtn =document.querySelector("#btn-confirm");
